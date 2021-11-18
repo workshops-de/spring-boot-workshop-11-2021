3e61c47e02051cfe31e53017ce5301bb28520a9e
Birgit Kratz <mail@birgitkratz.de>
2021-11-18 09:52:18 +0100

    Workshop Tag 3: create Book entity

--- src/main/java/de/workshops/bookshelf/app/Book.java
+++ src/main/java/de/workshops/bookshelf/app/Book.java
@@ -1,10 +1,25 @@
 package de.workshops.bookshelf.app;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+//@Table(name = "Buch")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String title;
+//    @Column(name = "beschreibung")
     private String description;
     private String author;
     private String isbn;
